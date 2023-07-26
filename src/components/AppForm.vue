6a6b260f2c0881c3eff46db74b14cf588d2416c8
domenicobacoli <domenico.bacoli89@gmail.com>
2023-07-26 20:01:24 +0200

    fix: test

--- src/components/AppForm.vue
+++ src/components/AppForm.vue
@@ -14,7 +14,7 @@ export default {
 <template>
 <!-- form di prova per invio di messaggi su netlify -->
 <div id="my-form">
-    <form name="/" data-netlify="true">
+    <form name="leafy-frangipane-a01c57" data-netlify="true">
         <p>
             <label>Your Name: <input type="text" name="name" /></label>
         </p>
