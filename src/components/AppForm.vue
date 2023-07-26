56ef7a8423868037110dca7a671381bbd1af3c9c
domenicobacoli <domenico.bacoli89@gmail.com>
2023-07-26 20:15:44 +0200

    test

--- src/components/AppForm.vue
+++ src/components/AppForm.vue
@@ -14,7 +14,7 @@ export default {
 <template>
 <!-- form di prova per invio di messaggi su netlify -->
 <div id="my-form">
-    <form name="contact-form" method="POST" data-netlify="true">
+    <form name="contact-form" action="/" method="POST" data-netlify="true">
         <p>
             <label>Your Name: <input type="text" name="name" /></label>
         </p>
