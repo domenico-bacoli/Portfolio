e55b1cb775a62076f0ace65a60cfffdf769d7095
domenicobacoli <domenico.bacoli89@gmail.com>
2023-07-26 22:52:07 +0200

    fix: form test

--- src/App.vue
+++ src/App.vue
@@ -22,43 +22,8 @@ export default {
 <template>
   <AppHeader></AppHeader>
   <router-view></router-view>
-  <div id="my-form">
-      <form name="contact" netlify>
-          <p>
-              <label>Your Name: <input type="text" name="name" /></label>
-          </p>
-          <p>
-              <label>Your Email: <input type="email" name="email" /></label>
-          </p>
-          <p>
-              <label>Message: <textarea name="message"></textarea></label>
-          </p>
-          <p>
-              <button type="submit">Send</button>
-          </p>
-      </form>
-    </div>
+
 </template>
 
 <style lang="scss" scoped>
-#my-form{
-    opacity: 0;
-    position: absolute;
-    top: 100%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    padding: 100px 200px;
-    background-color: #f7f9fb;
-    background-image: linear-gradient(62deg, #f7f9fb 0%, #fbfbff 100%);
-    box-shadow: 4px 4px 30px rgba(0, 0, 0, 0.2);
-    border-radius: 24px;
-    transition: opacity 0.5s ease, top 0.5s ease;
-}
-
-#my-form.visible{
-    opacity: 1;
-    top: 50%;
-    transform: translate(-50%, -50%);
-    z-index: 2;
-}
 </style>
