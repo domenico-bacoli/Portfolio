3b83a858782ced81fa1c4a6f970998a2604aa74b
domenicobacoli <domenico.bacoli89@gmail.com>
2023-07-26 17:53:34 +0200

    fix: AppAbout

--- src/pages/AppAbout.vue
+++ src/pages/AppAbout.vue
@@ -68,12 +68,18 @@ export default {
     </div>
     <!-- form di prova per invio di messaggi su netlify -->
     <div id="my-form">
-        <form name="contact" method="POST" action="/" data-netlify="true">
+        <form name="contact" method="POST" data-netlify="true">
             <p>
                 <label>Your Name: <input type="text" name="name" /></label>
             </p>
             <p>
                 <label>Your Email: <input type="email" name="email" /></label>
+            </p>
+            <p>
+                <label>Your Role: <select name="role[]" multiple>
+                <option value="leader">Leader</option>
+                <option value="follower">Follower</option>
+                </select></label>
             </p>
             <p>
                 <label>Message: <textarea name="message"></textarea></label>
@@ -82,6 +88,7 @@ export default {
                 <button type="submit">Send</button>
             </p>
         </form>
+
     </div>
 
 
