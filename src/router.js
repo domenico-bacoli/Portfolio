895a287b9b58297831ba38f19e5147ac30534eb6
domenicobacoli <domenico.bacoli89@gmail.com>
2023-07-26 16:12:22 +0200

    feat: title pages at router.js

--- src/router.js
+++ src/router.js
@@ -25,7 +25,7 @@ const router = createRouter({
             name: 'about',
             component: AppAbout,
             meta: {
-                title: 'Chi sono'
+                title: 'About me'
             }
         },
         
@@ -34,7 +34,7 @@ const router = createRouter({
             name: 'projects.index',
             component: ProjectsIndex,
             meta: {
-                title: 'Progetti'
+                title: 'Projects'
             }
         },
 
